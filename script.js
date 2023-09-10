5f438e60349646d51ade8285547d3ed5051736b0
Aija <aija.skele@gmail.com>
2023-09-10 16:52:24 +0300

    Finished prototype

--- script.js
+++ script.js
@@ -1,73 +1,96 @@
 let myLibrary = [];
+const table = document.querySelector("table");
+const tbody = document.querySelector("tbody");
 
-const body = document.querySelector("body");
+const form = document.querySelector("#addBook");
+const addBtn = document.querySelector("#addNewBook");
+const cancelBtn = document.querySelector(".cancel");
+const clearBtn = document.querySelector(".clear");
+addBtn.addEventListener("click", () => form.classList.remove("hide"));
 
-const form = body.querySelector("#addBook");
-const addBtn = body.querySelector("#addNewBook");
-addBtn.addEventListener("click", e => form.style.display = "block");
-
-const submit = body.querySelector("#submit");
-submit.addEventListener("click", addBookToLibrary);
-
-const table = document.createElement("table");
-let theadData = ["Title", "Author", "Page count", "Is Read?"];
-
-function createTable() {
-    let row = document.createElement("tr");
-    for (let i=0; i<theadData.length; i++) {
-        let thead = document.createElement("th");
-        thead.textContent = theadData[i];
-        row.appendChild(thead);
-        table.appendChild(row);
-    }   
-}
-
-createTable();
+form.addEventListener("submit", addBookToLibrary);
 
 function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
-    /*this.info = function () {
-        return (`${title} by ${author}, ${pages} pages, ${(read) ? "is read." : "not read yet."}`);
-    }*/
+}
+
+function renderLibrary() {
+    tbody.innerHTML = ""; // Clear the table before rendering
+    myLibrary.forEach((book, index) => {
+        const row = document.createElement("tr");
+
+        // Create cells for book details
+        const details = [book.title, book.author, book.pages];
+        details.forEach(detail => {
+            const cell = document.createElement("td");
+            cell.textContent = detail;
+            row.appendChild(cell);
+        });
+
+        // Create a cell for the "Is read" checkbox
+        const isReadCell = document.createElement("td");
+        const isReadCheckbox = document.createElement("input");
+        isReadCheckbox.type = "checkbox";
+        isReadCheckbox.checked = book.isRead;
+        isReadCheckbox.addEventListener("change", () => toggleReadStatus(index));
+        isReadCell.appendChild(isReadCheckbox);
+        row.appendChild(isReadCell);
+
+        // Create a cell for the "Remove" button
+        const removeCell = document.createElement("td");
+        const removeButton = document.createElement("button");
+        removeButton.textContent = "Remove book";
+        removeButton.addEventListener("click", () => removeBook(index));
+        removeCell.appendChild(removeButton);
+        row.appendChild(removeCell);
+
+        row.setAttribute("data-index", index);
+        tbody.appendChild(row);
+    });
 }
 
 function addBookToLibrary(e) {
     e.preventDefault();
-    const title = body.querySelector("#title").value;
-    const author = body.querySelector("#author").value;
-    const pages = body.querySelector("#pages").value;
-    const isRead = body.querySelector("#isRead").checked;
-    let tempLibrary = [];
-    tempLibrary.push(title, author, pages, isRead);
-    myLibrary.push(tempLibrary);
-    let row = document.createElement("tr");
-    for (let i=0; i<tempLibrary.length; i++) {
-        let tdata = document.createElement("td");
-        tdata.textContent = tempLibrary[i];
-        row.appendChild(tdata);
-        table.appendChild(row);
-    }
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const pages = document.querySelector("#pages").value;
+    const isRead = document.querySelector("#isRead").checked;
+
+    const newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+
+    form.reset();
+    renderLibrary(); // Render the updated library
 }
 
-/*Book.prototype.info = function () {
-    return (`${title} by ${author}, ${pages} pages, ${(isRread) ? "is read." : "not read yet."}`);
-}*/
-
-
-function displayLibrary(lib) {
-    body.appendChild(table);
-    lib.forEach(book => {
-        let row = document.createElement("tr");
-        for (let i=0; i<book.length; i++) {
-            let tdata = document.createElement("td");
-            tdata.textContent = book[i];
-            row.appendChild(tdata);
-            table.appendChild(row);   
-        }
-    });
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    renderLibrary(); // Render the updated library
 }
 
-displayLibrary(myLibrary);
+function toggleReadStatus(index) {
+    myLibrary[index].isRead = !myLibrary[index].isRead;
+    renderLibrary(); // Render the updated library
+}
+
+document.addEventListener("click", (e) => { 
+  if (!form.contains(e.target) && e.target !== addBtn) {
+    form.classList.add("hide"); //Close modal when user clicks outside the modal
+    form.reset();
+  }
+});
+
+cancelBtn.addEventListener("click", e => {
+    form.classList.add("hide");
+    form.reset();
+  });
+
+clearBtn.addEventListener("click", e => {
+    form.reset();
+});
+
+// Initialize the library by rendering it
+renderLibrary();
